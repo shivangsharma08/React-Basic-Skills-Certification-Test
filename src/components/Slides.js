d356166f582b10d7e8c97a97297cc7572425f2f0
User <user.push@hackerrank.workspace>
2021-05-16 13:35:38 +0000

    [Admin Commit] save changes
    Commit by HackerRank

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -2,17 +2,33 @@ import React from 'react';
 
 function Slides({slides}) {
 
+
+  function slidesNextHandler () {
+      
+    console.log('next clicked');
+    slides.map(function(s, index) {
+      return( 
+        <div id="slide" className="card text-center">
+          <h1 data-testid="title">{s.title}</h1>
+          <p data-testid="text">{s.text}</p>
+        </div>
+     
+        
+      );
+    })
+  }
     return (
         <div>
             <div id="navigation" className="text-center">
                 <button data-testid="button-restart" className="small outlined">Restart</button>
                 <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-next" className="small" onClick={slidesNextHandler}>Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">Today's Workout Plan</h1>
+                <p data-testid="text">We're gonna do 3 fundamental exercises.</p>
             </div>
+            
         </div>
     );
 
