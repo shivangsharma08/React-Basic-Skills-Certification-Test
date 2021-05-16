d20a5c69f0afa5d67550e491cee092c9feacaa81
Shivang Sharma <shivangsharma0818@gmail.com>
2021-05-16 20:26:04 +0530

    Completed HackerRank SlideShow App's Development

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import React from 'react';
-import './App.css';
-import 'h8k-components';
+import React from "react";
+import "./App.css";
+import "h8k-components";
 
-import Slides from './components/Slides';
+import Slides from "./components/Slides";
 const title = "Slideshow App";
 
-function App({slides}) {
-  slides=[
-    {
-      title: "Today's Workout Plan",
-      text: "We're gonna do 3 fundamental exercises."
-    },
-    {
-      title: "First, 10 push-ups",
-      text: "Do 10 reps. Remember about full range of motion. Don't rush."
-    },
-    {
-      title: "Next, 20 squats",
-      text: "Squats are important. Bend your knees carefully/"
-    }
-]
-console.log(slides)
-    return (
-        <div>
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="App">
-                <Slides slides={slides} />
-            </div>
-        </div>
-    );
+function App({ slides }) {
+  return (
+    <div>
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="App">
+        <Slides slides={slides} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,37 +1,65 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+function Slides({ slides }) {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
 
-function Slides({slides}) {
-
-
-  function slidesNextHandler () {
-      
-    console.log('next clicked');
-    slides.map(function(s, index) {
-      return( 
-        <div id="slide" className="card text-center">
-          <h1 data-testid="title">{s.title}</h1>
-          <p data-testid="text">{s.text}</p>
-        </div>
-     
-        
-      );
-    })
+  function slidesNextHandler() {
+    console.log("next clicked");
+    setCurrent(current === length - 1 ? length - 1 : current + 1);
   }
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small" onClick={slidesNextHandler}>Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Today's Workout Plan</h1>
-                <p data-testid="text">We're gonna do 3 fundamental exercises.</p>
-            </div>
-            
-        </div>
-    );
 
+  function slidesPrevHandler() {
+    console.log("prev clicked");
+    setCurrent(current === 0 ? 0 : current - 1);
+  }
+
+  function restartHandler() {
+    setCurrent(0);
+  }
+
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          onClick={restartHandler}
+          disabled={current === 0}
+        >
+          Restart
+        </button>
+        <button
+          data-testid="button-prev"
+          className="small"
+          onClick={slidesPrevHandler}
+          disabled={current === 0}
+        >
+          Prev
+        </button>
+        <button
+          data-testid="button-next"
+          className="small"
+          onClick={slidesNextHandler}
+          disabled={current === length - 1}
+        >
+          Next
+        </button>
+      </div>
+      {slides.map(function (s, index) {
+        return (
+          <div key={index}>
+            {index === current && (
+              <div id="slide" className="card text-center">
+                <h1 data-testid="title">{s.title}</h1>
+                <p data-testid="text">{s.text}</p>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Slides;
